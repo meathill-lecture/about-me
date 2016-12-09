5763be32e6a14456375b9c8ebe7109dfa5fc5c15
Meathill <meathill@gmail.com>
2016-12-09 17:57:13 +0800

    开始写第一课

---
+++ slides/lesson1/lesson-1.md
@@ -0,0 +1,25 @@
+### “个人网站”开发实战 第一课
+# 项目启动
+
+### 讲师：翟路佳
+
+--------
+
+## 自我介绍
+
+![翟路佳靓照](http://blog.meathill.com/wp-content/uploads/2016/07/20160607_005422423_iOS-825x510.jpg)<!-- .element: height="400" -->
+
+========
+
+## 我叫翟路佳
+
+### 花名肉山，英文名 Meathill
+
+热爱编程，喜欢分享。2006年开始做前端，切过图，写过 Flash 游戏。近几年创业，转向全栈。现居广州。
+
+<p class="social-links">
+  [<i class="fa fa-weibo"></i>](http://weibo.com/meathill)
+  [<i class="fa fa-github"></i>](https://github.com/meathill)
+  [<i class="fa fa-user"></i>](http://blog.meathill.com/)
+  [<i class="fa fa-envelope"></i>](mailto:meathill@gmail.com)
+</p>
---
+++ slides/slide.js
@@ -0,0 +1,52 @@
+/**
+ * Created by meathill on 2016/12/9.
+ */
+'use strict';
+
+Reveal.initialize({
+  controls: true,
+  progress: true,
+  history: true,
+  center: true,
+  slideNumber: true,
+
+  theme: Reveal.getQueryHash().theme,
+  transition: Reveal.getQueryHash().transition || 'default',
+
+  dependencies: [
+    {
+      src: '../../node_modules/marked/marked.min.js',
+      condition: function() {
+        return !!document.querySelector('[data-markdown]');
+      }
+    },
+    {
+      src: '../../node_modules/reveal.js/plugin/markdown/markdown.js',
+      condition: function() {
+        return !!document.querySelector('[data-markdown]');
+      }
+    },
+    {
+      src: '../../node_modules/reveal.js/plugin/highlight/highlight.js',
+      async: true,
+      callback: function () {
+        hljs.initHighlightingOnLoad();
+      }
+    },
+    {
+      src: '../../node_modules/reveal.js/plugin/notes/notes.js',
+      async: true
+    }
+  ]
+});
+
+// QQ 统计
+( function(id) {
+  var script = document.createElement('script');
+  script.src = 'http://tajs.qq.com/stats?sId=' + id;
+  script.charset = 'UTF-8';
+  script.async = 1;
+  var scripts = document.getElementsByTagName('script');
+  var last = scripts[scripts.length - 1];
+  last.parentNode.insertBefore(script, last);
+}(59876390));
